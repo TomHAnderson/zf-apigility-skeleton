e4462f09f76c6d8634c741b620bb94911baba1af
Tom Anderson <tom.h.anderson@gmail.com>
2015-08-02 12:31:51 -0600

    Added oauth2 config

---
+++ config/autoload/global.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'oauth' => array(
+                'options' => array(
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ),
+                'type' => 'regex',
+            ),
+        ),
+    ),
+    'zf-mvc-auth' => array(
+        'authentication' => array(
+            'map' => array(
+                'DbApi\\V1' => 'oauth2_doctrine',
+            ),
+        ),
+    ),
+);
